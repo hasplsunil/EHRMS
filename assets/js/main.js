296e2bf796586ca8d8941ab8399a2442baf58b42
hasplsunil <epaysunil@gmail.com>
2021-08-11 13:26:01 +0530

    mail function added

---
+++ assets/js/main.js
@@ -0,0 +1,477 @@
+
+(function() {
+  "use strict";
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim()
+    if (all) {
+      return [...document.querySelectorAll(el)]
+    } else {
+      return document.querySelector(el)
+    }
+  }
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all)
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach(e => e.addEventListener(type, listener))
+      } else {
+        selectEl.addEventListener(type, listener)
+      }
+    }
+  }
+
+  /**
+   * Easy on scroll event listener 
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener)
+  }
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true)
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200
+    navbarlinks.forEach(navbarlink => {
+      if (!navbarlink.hash) return
+      let section = select(navbarlink.hash)
+      if (!section) return
+      if (position >= section.offsetTop && position <= (section.offsetTop + section.offsetHeight)) {
+        navbarlink.classList.add('active')
+      } else {
+        navbarlink.classList.remove('active')
+      }
+    })
+  }
+  window.addEventListener('load', navbarlinksActive)
+  onscroll(document, navbarlinksActive)
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header')
+    let offset = header.offsetHeight
+
+    let elementPos = select(el).offsetTop
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth'
+    })
+  }
+
+  /**
+   * Toggle .header-scrolled class to #header when page is scrolled
+   */
+  let selectHeader = select('#header')
+  if (selectHeader) {
+    const headerScrolled = () => {
+      if (window.scrollY > 100) {
+        selectHeader.classList.add('header-scrolled')
+      } else {
+        selectHeader.classList.remove('header-scrolled')
+      }
+    }
+    window.addEventListener('load', headerScrolled)
+    onscroll(document, headerScrolled)
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top')
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active')
+      } else {
+        backtotop.classList.remove('active')
+      }
+    }
+    window.addEventListener('load', toggleBacktotop)
+    onscroll(document, toggleBacktotop)
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function(e) {
+    select('#navbar').classList.toggle('navbar-mobile')
+    this.classList.toggle('bi-list')
+    this.classList.toggle('bi-x')
+  })
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on('click', '.navbar .dropdown > a', function(e) {
+    if (select('#navbar').classList.contains('navbar-mobile')) {
+      e.preventDefault()
+      this.nextElementSibling.classList.toggle('dropdown-active')
+    }
+  }, true)
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on('click', '.scrollto', function(e) {
+    if (select(this.hash)) {
+      e.preventDefault()
+
+      let navbar = select('#navbar')
+      if (navbar.classList.contains('navbar-mobile')) {
+        navbar.classList.remove('navbar-mobile')
+        let navbarToggle = select('.mobile-nav-toggle')
+        navbarToggle.classList.toggle('bi-list')
+        navbarToggle.classList.toggle('bi-x')
+      }
+      scrollto(this.hash)
+    }
+  }, true)
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash)
+      }
+    }
+  });
+
+  /**
+   * Preloader
+   */
+  let preloader = select('#preloader');
+  if (preloader) {
+    window.addEventListener('load', () => {
+      preloader.remove()
+    });
+  }
+
+  /**
+   * Initiate  glightbox 
+   */
+  const glightbox = GLightbox({
+    selector: '.glightbox'
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function(direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%'
+        });
+      }
+    })
+  }
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item'
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on('click', '#portfolio-flters li', function(e) {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el) {
+          el.classList.remove('filter-active');
+        });
+        this.classList.add('filter-active');
+
+        portfolioIsotope.arrange({
+          filter: this.getAttribute('data-filter')
+        });
+        portfolioIsotope.on('arrangeComplete', function() {
+          AOS.refresh()
+        });
+      }, true);
+    }
+
+  });
+
+  /**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox'
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    speed: 400,
+    loop: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    }
+  });
+
+  /**
+   * Animation on scroll
+   */
+  window.addEventListener('load', () => {
+    AOS.init({
+      duration: 1000,
+      easing: "ease-in-out",
+      once: true,
+      mirror: false
+    });
+  });
+
+})()  
+
+
+//modal 
+
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+//for each modal
+//ser 1
+var readmore = document.getElementsByClassName("employee Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 2
+var readmore = document.getElementsByClassName("timecard Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 3
+var readmore = document.getElementsByClassName("shift Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 4
+var readmore = document.getElementsByClassName("attendance Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 5
+var readmore = document.getElementsByClassName("payroll Processing")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 6
+var readmore = document.getElementsByClassName("employee Self Servics")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 7
+var readmore = document.getElementsByClassName("employee Onboarding")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//ser 8
+var readmore = document.getElementsByClassName("ctc")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 9
+var readmore = document.getElementsByClassName("employeee Case Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 10
+var readmore = document.getElementsByClassName("employeeee Asset Tracking Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 11
+var readmore = document.getElementsByClassName("employe Self Portal")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 12
+var readmore = document.getElementsByClassName("kpi")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 13
+var readmore = document.getElementsByClassName("human Capital Management")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 14
+var readmore = document.getElementsByClassName("training Development")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+
+//ser 15
+var readmore = document.getElementsByClassName("recruitment")[0];
+readmore.onclick = function () {
+  modal.style.display = "block";
+}
+
+//pricing toggle
+
+/*function check(){
+    var checkBox = document.getElementById("checkbox");
+    var checkBox = document.getElementById("rs1");
+    var checkBox = document.getElementById("rs2");
+
+    for (var i=0; i < rs1.length; i++){
+      if(checkBox.checked == true){
+        rs1[i].style.display == "block";
+        rs2[i].style.display == "none";
+       }
+       else if (checkBox.checked == false){
+        rs1[i].style.display == "none";
+        rs2[i].style.display == "block";
+       }
+    }
+}
+
+check(); 
+
+html
+ <div class="top">
+      <div class="toggle-btn">
+         <span style="margin: 0.8em;" >IN INDIA</span>
+         <label class="switch">
+           <span></span>
+           <input type="checkbox" id="checkbox" onclick="check()">
+           <span class="slider round"></span>
+         </label>
+         <span style="margin: 0.8em;">REST OF INDIA</span>
+      </div>
+    </div>
+css
+.top {
+  padding-top: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 60px;
+  height: 34px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #1e2321;
+  transform: translate(0px, 0px);
+  transition: 0.6s ease transform, 0.6s box-shadow;
+}
+
+.slider:before {
+  position: absolute;
+  content: '';
+  height: 26px;
+  width: 26px;
+  left: 4px;
+  bottom: 4px;
+  background-color: #fff;
+  transition: 0.4s;
+}
+
+input:checked + .slider {
+  background-color: #00cc99;
+}
+
+input:focus + .slider {
+  box-shadow: 0 0 1px #50bfe6;
+}
+
+input:checked + .slider:before {
+ transform: translateX(26px);
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+*/
+
+
+
+
+
+
+
