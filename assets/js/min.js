1b413599a9a5fe5ea41c05e82f5f06dcd27adad2
hasplsunil <epaysunil@gmail.com>
2021-08-21 14:27:02 +0530

    working signup

--- assets/js/min.js
+++ assets/js/min.js
@@ -1,26 +1,29 @@
-/*===== LOGIN SHOW and HIDDEN =====*/
-const signUp = document.getElementById('sign-up'),
-    signIn = document.getElementById('sign-in'),
-    loginIn = document.getElementById('login-in'),
-    loginUp = document.getElementById('login-up')
+// /*===== LOGIN SHOW and HIDDEN =====*/
+// const signUp = document.getElementById('sign-up'),
+//     signIn = document.getElementById('sign-in'),
+//     loginIn = document.getElementById('login-in'),
+//     loginUp = document.getElementById('login-up')
 
 
-signUp.addEventListener('click', ()=>{
-    // Remove classes first if they exist
-    loginIn.classList.remove('block')
-    loginUp.classList.remove('none')
+// signUp.addEventListener('click', ()=>{
+//     // Remove classes first if they exist
+//     loginIn.classList.remove('block')
+//     loginUp.classList.remove('none')
 
-    // Add classes
-    loginIn.classList.toggle('none')
-    loginUp.classList.toggle('block')
-})
+//     // Add classes
+//     loginIn.classList.toggle('block')
+//     loginUp.classList.toggle('none')
+// })
 
-signIn.addEventListener('click', ()=>{
-    // Remove classes first if they exist
-    loginIn.classList.remove('none')
-    loginUp.classList.remove('block')
+// signIn.addEventListener('click', ()=>{
+//     // Remove classes first if they exist
+//      loginIn.classList.remove('block')
+//      loginUp.classList.remove('block')
 
-    // Add classes
-    loginIn.classList.toggle('block')
-    loginUp.classList.toggle('none')
-})
+//     // Add classes
+//     loginIn.classList.toggle('block')
+//     loginUp.classList.toggle('block')
+// })
+
+
+
